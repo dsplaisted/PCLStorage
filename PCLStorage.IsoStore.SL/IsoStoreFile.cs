cc74e0b724462882d2ce51261a16bb4ba4fc1bcf
Daniel Plaisted <dsplaisted@gmail.com>
2013-02-26 01:06:48 -0800

    Start of Silverlight Isolated Storage implementation of storage interfaces

---
+++ PCLStorage.IsoStore.SL/IsoStoreFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PCLStorage
+{
+	public class IsoStoreFile : IFile
+	{
+		readonly string _name;
+		readonly IsoStoreFolder _parentFolder;
+		readonly Lazy<string> _path;
+
+		public IsoStoreFile(string name, IsoStoreFolder parentFolder)
+		{
+			_name = name;
+			_parentFolder = parentFolder;
+			_path = new Lazy<string>(() => System.IO.Path.Combine(_parentFolder.Path, _name));
+		}
+
+		public string Name
+		{
+			get { return _name; }
+		}
+
+		public string Path
+		{
+			get { return _path.Value; }
+		}
+
+		public Task<Stream> OpenAsync(FileAccess fileAccess)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task DeleteAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ PCLStorage.IsoStore.SL/IsoStoreFolder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PCLStorage
+{
+	public class IsoStoreFolder : IFolder
+	{
+
+		public string Name
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public string Path
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public System.Threading.Tasks.Task<IFile> CreateFileAsync(string desiredName, CreationCollisionOption option)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<IFile> GetFileAsync(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<System.Collections.Generic.IList<IFile>> GetFilesAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<IFolder> CreateFolderAsync(string desiredName, CreationCollisionOption option)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<IFolder> GetFolderAsync(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<System.Collections.Generic.IList<IFolder>> GetFoldersAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task DeleteAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal Task<Stream> OpenFileAsync(string filename, FileAccess fileAccess)
+		{
+
+
+			return TaskEx.FromResult<Stream>(null);
+		}
+	}
+}
