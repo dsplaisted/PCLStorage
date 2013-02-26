02c41ad47ada00ac58141fa20eac1f9cbb4f4a98
Daniel Plaisted <dsplaisted@gmail.com>
2013-02-26 01:05:37 -0800

    Portable storage interfaces and project

---
+++ PCLStorage/IFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCLStorage
+{
+	public enum FileAccess
+	{
+		Read,
+		ReadAndWrite
+	}
+
+	public interface IFile
+	{
+		string Name { get; }
+		string Path { get; }
+
+		Task<Stream> OpenAsync(FileAccess fileAccess);
+		Task DeleteAsync();
+	}
+}
---
+++ PCLStorage/IFolder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCLStorage
+{
+	public enum CreationCollisionOption
+	{
+		GenerateUniqueName = 0,
+		ReplaceExisting = 1,
+		FailIfExists = 2,
+		OpenIfExists = 3,
+	}
+
+	public interface IFolder
+	{
+		string Name { get; }
+		string Path { get; }
+
+		Task<IFile> CreateFileAsync(string desiredName, CreationCollisionOption option);
+		Task<IFile> GetFileAsync(string name);
+		Task<IList<IFile>> GetFilesAsync();
+
+		Task<IFolder> CreateFolderAsync(string desiredName, CreationCollisionOption option);
+		Task<IFolder> GetFolderAsync(string name);
+		Task<IList<IFolder>> GetFoldersAsync();
+
+		Task DeleteAsync();
+	}
+}
