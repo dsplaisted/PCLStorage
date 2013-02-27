1b1e2716ed132314b785ce5b72e293d11cc0887d
Daniel Plaisted <dsplaisted@gmail.com>
2013-02-27 08:35:25 -0800

    Added static Storage class to provide access to AppLocalStorage.

---
+++ PCLStorage/Storage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCLStorage
+{
+	public static class Storage
+	{
+		public static IFolder AppLocalStorage
+		{
+			get
+			{
+#if SILVERLIGHT
+				return new IsoStoreFolder(System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication());
+#elif NETFX_CORE
+				return new WinRTFolder(Windows.Storage.ApplicationData.Current.LocalFolder);
+#else
+				throw new NotImplementedException();
+#endif
+			}
+		}
+	}
+}
