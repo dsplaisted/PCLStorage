67e273db3397af75b752edde27d0527cd21f7d3f
Daniel Plaisted <dsplaisted@gmail.com>
2013-02-27 09:43:12 -0800

    Test framework library with test attributes and asserts, test project with one test.

---
+++ test/PCLStorage.Test/FileTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCLStorage.TestFramework;
+
+namespace PCLStorage.Test
+{
+	[TestClass]
+    public class FileTests
+    {
+		[TestMethod]
+		public static async Task GetFileThrowsWhenFileDoesNotExist()
+		{
+			string fileName = Guid.NewGuid().ToString();
+			IFolder folder = Storage.AppLocalStorage;
+			await Assert.ThrowsAsync<IOException>(async () => await folder.GetFileAsync(fileName));
+		}
+    }
+}
---
+++ test/PCLStorage.TestFramework/Attributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCLStorage.TestFramework
+{
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple=false)]
+	public class TestMethodAttribute : Attribute
+	{
+
+	}
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple=false)]
+	public class TestClassAttribute : Attribute
+	{
+
+	}
+}
