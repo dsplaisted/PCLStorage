d1e5672e7c0817bf5813945b70d3cf7ea520a100
Daniel Plaisted <dsplaisted@gmail.com>
2013-03-05 22:49:13 -0800

    Fixed spelling for WinRT test project: Storeage -> Storage

--- test/PCLStorage.Test.WinRT/Properties/AssemblyInfo.cs
+++ test/PCLStorage.Test.WinRT/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PCLStoreage.Test.WinRT")]
+[assembly: AssemblyTitle("PCLStorage.Test.WinRT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PCLStoreage.Test.WinRT")]
+[assembly: AssemblyProduct("PCLStorage.Test.WinRT")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
