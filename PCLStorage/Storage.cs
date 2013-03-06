3affbed5c55b7926b51d386be52465d21779fd02
Daniel Plaisted <dsplaisted@gmail.com>
2013-03-06 08:17:53 -0800

    Added better error message for portable assembly NotImplementedException.

--- PCLStorage/Storage.cs
+++ PCLStorage/Storage.cs
@@ -28,7 +28,7 @@ namespace PCLStorage
 
         internal static Exception NotImplementedInReferenceAssembly()
         {
-            return new NotImplementedException();
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the PCLStorage NuGet package from your main application project in order to reference the platform-specific implementation.");
         }
 	}
 }
