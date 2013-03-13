2e274f299bbd85a609094106e07d9c1b75fac5a6
Daniel Plaisted <dsplaisted@gmail.com>
2013-03-13 08:50:02 -0700

    Added IFileSystem interface

---
+++ src/PCLStorage/IFileSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCLStorage
+{
+    public interface IFileSystem
+    {
+        IFolder LocalStorage { get; }
+        IFolder RoamingStorage { get; }
+
+        bool FileExists(string path);
+        bool FolderExists(string path);
+
+        IFile GetFileFromPath(string path);
+        IFolder GetFolderFromPath(string path);
+    }
+}
