3b96669b34a4ed8b3b958e671e91b9d787d03ea2
Daniel Plaisted <dsplaisted@gmail.com>
2013-04-21 11:47:14 -0700

    Moved FileExtensions to abstractions library and added XML comments.

--- src/PCLStorage.Abstractions/FileExtensions.cs
+++ src/PCLStorage.Abstractions/FileExtensions.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace PCLStorage
 {
+    /// <summary>
+    /// Provides extension methods for the <see cref="IFile"/> class
+    /// </summary>
     public static class FileExtensions
     {
+        /// <summary>
+        /// Reads the contents of a file as a string
+        /// </summary>
+        /// <param name="file">The file to read </param>
+        /// <returns>The contents of the file</returns>
         public static async Task<string> ReadAllTextAsync(this IFile file)
         {
             using (var stream = await file.OpenAsync(FileAccess.Read))
@@ -21,6 +29,12 @@ namespace PCLStorage
             }
         }
 
+        /// <summary>
+        /// Writes text to a file, overwriting any existing data
+        /// </summary>
+        /// <param name="file">The file to write to</param>
+        /// <param name="contents">The content to write to the file</param>
+        /// <returns>A task which completes when the write operation finishes</returns>
         public static async Task WriteAllTextAsync(this IFile file, string contents)
         {
             using (var stream = await file.OpenAsync(FileAccess.ReadAndWrite))
