f7c468601524dfad0255af8962d3309b7982d975
Andrew Arnott <andrewarnott@gmail.com>
2014-01-24 06:51:11 -0800

    Adds a couple tests demonstrating how apps that must execute synchronously would call into this library, including a simulation of how the main message pump works.
    The synchronous test would hang without the .ConfigureAwait(false) changes made in the prior commit, if WinRT happens to decide to yield in their async method, which I've found happens intermittently.
    
    A yield could be forced in one of these async methods of the library by adding this line:
    await Task.Delay(0);
    Adding this line to one of the methods called by this test would cause the test to hang. Modifying this line to instead read:
    await Task.Delay(0).ConfigureAwait(false);
    resolves the hang, demonstrating what the effect of ConfigureAwait(false) does.
    
    Note however that every line in the test code actually runs on the main thread. The only code that might run on a threadpool thread is the code that is internal to this library, which has no thread-safety requirements anyway (at least none that were fulfilled before this change).

---
+++ test/PCLStorage.Test.WinRT/FileTestsForDesktop.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PCLTesting;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+
+namespace PCLStorage.Test
+{
+	[TestClass]
+    public class FileTestsForDesktop
+    {
+        IFileSystem TestFileSystem { get { return FileSystem.Current; } }
+
+        [TestMethod]
+        public void OpenFileForRead_NoHangForAsync()
+        {
+            // Simulate the UI thread of an app.
+            UIDispatcherMock.MainThreadEntrypoint(async delegate
+            {
+                //  Arrange
+                IFolder folder = TestFileSystem.LocalStorage;
+                string fileName = "fileToOpenForRead.txt";
+                IFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.FailIfExists);
+
+                //  Act
+                using (Stream stream = await file.OpenAsync(FileAccess.Read))
+                {
+
+                    //  Assert
+                    Assert.IsFalse(stream.CanWrite);
+                    Assert.IsTrue(stream.CanRead);
+                    Assert.IsTrue(stream.CanSeek);
+                }
+
+                //  Cleanup
+                await file.DeleteAsync();
+            });
+        }
+
+        [TestMethod]
+        public void OpenFileForRead_NoHangForSync()
+        {
+            // Simulate the UI thread of an app.
+            UIDispatcherMock.MainThreadEntrypoint(delegate
+            {
+                //  Arrange
+                IFolder folder = TestFileSystem.LocalStorage;
+                string fileName = "fileToOpenForRead.txt";
+                IFile file = folder.CreateFileAsync(fileName, CreationCollisionOption.FailIfExists).Result;
+
+                //  Act
+                using (Stream stream = file.OpenAsync(FileAccess.Read).Result)
+                {
+
+                    //  Assert
+                    Assert.IsFalse(stream.CanWrite);
+                    Assert.IsTrue(stream.CanRead);
+                    Assert.IsTrue(stream.CanSeek);
+                }
+
+                //  Cleanup
+                file.DeleteAsync().Wait();
+            });
+        }
+    }
+}
