e006193a32e631dc2d4d91ffe82f47cf1e60d72a
Paul Laplante <pllplnt2@gmail.com>
2017-09-20 16:56:37 -0400

    Changed location of DocumentStorage on Android

--- src/PCLStorage.FileSystem.Desktop/DesktopFileSystem.cs
+++ src/PCLStorage.FileSystem.Desktop/DesktopFileSystem.cs
@@ -44,7 +44,7 @@ namespace PCLStorage
                 //  SpecialFolder.LocalApplicationData is not app-specific, so use the Windows Forms API to get the app data path
                 //var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
 #if ANDROID
-                var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
 #elif IOS
                 var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 #else
